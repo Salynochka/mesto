68bbed0de68ff7504cfd4e7c44301c6c4ba45a92
Alena Salynskaia <al_20.07@mail.ru>
2023-02-18 12:48:52 +0500

    Full project 1 review

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,26 @@
 let formElement = document.querySelector('.profile__changes');
-
-formElement.addEventListener('click', function handleFormSubmit (evt) {
-    evt.preventDefault();
-    let editProfile = document.querySelector('.popup');
-    editProfile.classList.add('popup_opened');
-    console.log(editProfile);
-});
-
 let closeButton = document.querySelector('.popup__button-close');
-
-closeButton.addEventListener('click', function handleFormClose (evt) {
-    evt.preventDefault();
-    let closeProfile = document.querySelector('.popup');
-    closeProfile.classList.remove('popup_opened');
-    console.log(closeProfile);
-});
-
-let saveButton = document.querySelector('.popup__button-save');
+let formButton = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__item_type_name');
 let jobInput = document.querySelector('.popup__item_type_job');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 
-saveButton.addEventListener('click', function handleFormSave (evt) {
+formElement.addEventListener('click', function handleFormSubmit () {
+    let editProfile = document.querySelector('.popup');
+    editProfile.classList.add('popup_opened');
+});
+
+closeButton.addEventListener('click', function handleFormClose () {
+    let closeProfile = document.querySelector('.popup');
+    closeProfile.classList.remove('popup_opened');
+});
+
+nameInput.value = profileName.textContent;
+jobInput.value = profileDescription.textContent;
+
+formButton.addEventListener('submit', function handleFormSave (evt) {
     evt.preventDefault();
-    console.log(nameInput.value);
-    console.log(jobInput.value);  
     profileName.textContent=nameInput.value;
     profileDescription.textContent=jobInput.value;
     let saveProfile = document.querySelector('.popup');
